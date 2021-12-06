ba4e1789fd6ac4f8ad612c54421512b5f22fa038
Kamil Śliwak <kamil.sliwak@codepoets.it>
2021-12-06 16:34:36 +0100

    External test for sushiswap/trident

--- test/externalTests.sh
+++ test/externalTests.sh
@@ -47,6 +47,7 @@ printTask "Running external tests..."
 "$REPO_ROOT/externalTests/gnosis-v2.sh" "$SOLJSON"
 "$REPO_ROOT/externalTests/colony.sh" "$SOLJSON"
 "$REPO_ROOT/externalTests/ens.sh" "$SOLJSON"
+"$REPO_ROOT/externalTests/trident.sh" "$SOLJSON"
 
 # Disabled temporarily as it needs to be updated to latest Truffle first.
 #test_truffle Gnosis https://github.com/axic/pm-contracts.git solidity-050
